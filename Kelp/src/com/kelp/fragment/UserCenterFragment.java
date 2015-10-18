1e204bac1e989d7ac89bf563cd138f343fe63194
姜浩 <haojiang@sohu-inc.com>
2015-10-18 17:45:34 +0800

    modify usercenterfragment

--- Kelp/src/com/kelp/fragment/UserCenterFragment.java
+++ Kelp/src/com/kelp/fragment/UserCenterFragment.java
@@ -1,7 +1,19 @@
 package com.kelp.fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class UserCenterFragment extends Fragment {
-	
+	private ListView optionListView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
 }
